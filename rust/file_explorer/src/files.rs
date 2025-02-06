b3cf763e10a7213bb5c3ee30d8b8116dfcaa1d98
ayaanwaqas <ayaanwaqas@googlemail.com>
2025-02-06 11:18:19 +0000

    fixed file printing

--- rust/file_explorer/src/files.rs
+++ rust/file_explorer/src/files.rs
@@ -1,4 +1,4 @@
-use std::io::Stdout;
+use std::io::{ Stdout, Write };
 use std::path::PathBuf;
 use crossterm::execute;
 use crossterm::style::{
@@ -9,14 +9,13 @@ use crossterm::style::{
 };
 use crate::term::{ self, PathData };
 
-pub fn main_section_files( screen: &Stdout, path: PathBuf, x: u16 ) -> Vec<PathData> {
-    let dir = std::fs::read_dir( path );
-    let paths: Vec<PathData> = vec![];
-    dir.iter().map(
-        | subpath |
-        paths.push( PathData::new( subpath ) )
-    );
-    let path_index = 0;
+pub fn main_section_files( mut screen: &Stdout, path: PathBuf, x: u16 ) -> Vec<PathData> {
+    let dir = std::fs::read_dir( path ).unwrap();
+    let mut paths: Vec<PathData> = vec![];
+    for path in dir {
+        paths.push( PathData::new( path.unwrap().path() ) );
+    }
+    let mut path_index = 0;
 
     for path in &paths {
         term::move_cursor( screen, x, path_index );
@@ -28,7 +27,9 @@ pub fn main_section_files( screen: &Stdout, path: PathBuf, x: u16 ) -> Vec<PathD
             ResetColor,
             SetAttribute( Attribute::Reset )
         ).unwrap();
+        path_index += 1;
     }
+    screen.flush().unwrap();
 
     paths
 }
